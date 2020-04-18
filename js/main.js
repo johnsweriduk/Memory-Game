64e8b264fa97b8ecc89fc2817654dd811e0a6ae1
John Sweriduk <john@stackandsizzle.com>
2020-04-18 14:37:58 -0700

    created game logic - objects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-let cards = ['queen','queen','king','king'];
+let cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts';
+		cardImage: 'images/queen-of-hearts.png'
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds';
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+	{
+		rank: 'king',
+		suit: 'hearts';
+		cardImage: 'images/king-of-hearts.png'
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds';
+		cardImage: 'images/king-of-diamonds.png'
+	}
+];
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -12,8 +33,10 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-	console.log("User flipped" + cards[cardId]);
-	cardsInPlay.push(card[cardId]);
+	console.log("User flipped" + cards[cardId].rank);
+	console.log("Suit is " + cards[cardId].suit);
+	console.log("Card Image is " + cards[cardId].cardImage);
+	cardsInPlay.push(card[cardId].rank);
 	if(cardsInPlay.length === 2) {
 		checkForMatch();
 	}
