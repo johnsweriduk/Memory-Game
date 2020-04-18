cb54b0f0f9c2b504221744536a6b577d997b8e11
John Sweriduk <john@stackandsizzle.com>
2020-04-18 14:19:21 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-let cardOne = 'queen';
-let cardTwo = 'king';
-let cardThree = 'king';
-let cardFour = 'queen';
+let cards = ['queen','queen','king','king'];
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2) {
+	if(cardOne === cardTwo) {
+		alert("You Found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
